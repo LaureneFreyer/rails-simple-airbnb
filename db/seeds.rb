156ea7686a5c0d070cf4ab810b14c31b82787801
Laurene Freyer <laurene.lepvrier@hotmail.fr>
2023-06-14 16:13:51 +0200

    done w/o css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  url: "https://images.unsplash.com/photo-1523217582562-09d0def993a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80"
+)
+Flat.create!(
+  name: 'Country chateau in Loire Valley',
+  address: 'Le Boulay, Centre',
+  description: 'Set amidst 200 acres, this XVI century property overlooking its own lake offers: On site: french and english gardens, park, swimming pool, croquet, badminton, billiard, farm with poultry, horses, vegetable garden, fruit trees.',
+  price_per_night: 601,
+  number_of_guests: 12,
+  url: "https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+)
+Flat.create!(
+  name: 'Exceptional design estate near Chambord Castle',
+  address: 'Pruniers-en-Sologne, Centre-Val de Loire',
+  description: 'Enjoy an unforgettable experience in a stunning design near Chambord. The Oranga Retreats estate, ideally located near the famous Châteaux de la Loire, offers you a unique combination of luxury and nature.',
+  price_per_night: 858,
+  number_of_guests: 16,
+  url: "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2074&q=80"
+)
+Flat.create!(
+  name: 'Delightful earth romantic room20 mn from Paris',
+  address: 'Epinay-sur-Orge, Île-de-France',
+  description: "You'll love this unique and romantic escape under the earth, in an unique master house with pool and garden.",
+  price_per_night: 82,
+  number_of_guests: 2,
+  url: "https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+)
